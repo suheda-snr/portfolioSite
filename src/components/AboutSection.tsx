7c3d050a8a39f136b7c6ece96fa9e3d31f3f76f3
suheda-snr <t3sesu00@students.oamk.fi>
2025-08-03 19:17:25 +0300

    improve the overall structure and complete AboutSection

--- src/components/AboutSection.tsx
+++ src/components/AboutSection.tsx
@@ -1,16 +1,33 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import '../styles/AboutSection.css';
 
 const AboutSection: React.FC = () => {
+    const { t } = useTranslation();
+    const techList = t('about.techList', { returnObjects: true }) as string[];
+
     return (
         <div className="about-wrapper">
-            <div className="container about-section py-5 px-4 px-md-5">
-                <div className="row align-items-center">
-                    This is the About Section.
+            <div className="container about-section px-md-5" >
+                <div className="row">
+                    <div className="col">
+                        <h2 className="mb-4">{t('about.title')}</h2>
+                        <div className="content-wrapper">
+                            <p>{t('about.description')}</p>
+                            <p>{t('about.story')}</p>
+                            <p>{t('about.experience')}</p>
+                            <p><strong>{t('about.techIntro')}</strong></p>
+                            <ul className="tech-list">
+                                {techList.map((tech, index) => (
+                                    <li key={index}>{tech}</li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     );
-}
+};
 
 export default AboutSection;
